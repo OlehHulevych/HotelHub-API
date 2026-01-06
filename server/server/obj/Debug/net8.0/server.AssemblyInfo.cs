c987d0e83970f34c66e7e412e7e72ad2eb03fcc9
Oleh <oleggulevic8@gmail.com>
2026-01-06 21:22:34 +0100

    fixing of getting details

--- server/server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server/server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6965190a6950aa8fb5d28d1f67412ec2f19a02ee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b34538ae6408ca770dacdd719cc8edace8e11b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
